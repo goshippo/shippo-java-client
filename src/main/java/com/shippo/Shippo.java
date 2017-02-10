e53d3e67b4ce4542c7d4228fcb79766c5a0bd163
Manish Tomar <manish@goshippo.com>
2017-02-10 15:41:01 -0800

    disable debug
    that was enabled by mistake in last commit

--- src/main/java/com/shippo/Shippo.java
+++ src/main/java/com/shippo/Shippo.java
@@ -4,7 +4,7 @@ public abstract class Shippo {
 
     public static final String LIVE_API_BASE = "https://api.goshippo.com";
     public static final String VERSION = "1.0";
-    public static boolean DEBUG = true;
+    public static boolean DEBUG = false;
 
     public static int RATES_REQ_TIMEOUT = 25000; //  milliseconds
     public static int TRANSACTION_REQ_TIMEOUT = 25000; // milliseconds

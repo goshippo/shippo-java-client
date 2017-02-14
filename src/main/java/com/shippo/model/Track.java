2705590c3eefd216bf5d3d82770c9833b4be6732
Manish Tomar <manish@goshippo.com>
2017-02-14 13:30:50 -0800

    format with tabs as spaces
    this should fix any wierd indentation issues

--- src/main/java/com/shippo/model/Track.java
+++ src/main/java/com/shippo/model/Track.java
@@ -17,66 +17,68 @@ import com.shippo.exception.InvalidRequestException;
 import com.shippo.exception.RequestTimeoutException;
 import com.shippo.net.APIResource;
 
-
 /**
- * Use this class to track shipments. Represents object defined in https://api.goshippo.com/tracks endpoint.
- * The endpoint's documentation can be found in https://goshippo.com/docs/reference#tracks
+ * Use this class to track shipments. Represents object defined in
+ * https://api.goshippo.com/tracks endpoint. The endpoint's documentation can be
+ * found in https://goshippo.com/docs/reference#tracks
  */
 public final class Track extends APIResource {
-    
+
 	private String carrier;
-    private String trackingNumber;
-    
-    public static class Address {
-        private String city;
-        private String state;
-        private String zip;
-        private String country;
-		
-        @Override
+	private String trackingNumber;
+
+	public static class Address {
+		private String city;
+		private String state;
+		private String zip;
+		private String country;
+
+		@Override
 		public String toString() {
 			return "Address [city=" + city + ", state=" + state + ", zip=" + zip + ", country=" + country + "]";
 		}
-    }
+	}
 
-    private Address addressFrom;
-    private Address addressTo;
-    private Date eta;
-    
-    public static class ServiceLevel {
-    	private String token;
-    	private String name;
-    	
-        public String getToken() {
+	private Address addressFrom;
+	private Address addressTo;
+	private Date eta;
+
+	public static class ServiceLevel {
+		private String token;
+		private String name;
+
+		public String getToken() {
 			return token;
 		}
+
 		public String getName() {
 			return name;
 		}
+
 		@Override
 		public String toString() {
 			return "ServiceLevel [token=" + token + ", name=" + name + "]";
 		}
-    }
-    
-    @SerializedName("servicelevel")
-    private ServiceLevel serviceLevel;
+	}
 
-    private String metadata;
+	@SerializedName("servicelevel")
+	private ServiceLevel serviceLevel;
 
-    public static enum TrackingStatus {
-        UNKNOWN, DELIVERED, TRANSIT, FAILURE, RETURNED 
-    }
+	private String metadata;
 
-    public static class TrackingEvent {
-        private Date objectCreated;
-        private Date objectUpdated;
-        private String objectId;
-        private TrackingStatus status;
-        private String statusDetails;
-        private Date statusDate;
-        private Address location;
-        
+	public static enum TrackingStatus {
+		UNKNOWN, DELIVERED, TRANSIT, FAILURE, RETURNED
+	}
+
+	public static class TrackingEvent {
+		private Date objectCreated;
+		private Date objectUpdated;
+		private String objectId;
+		private TrackingStatus status;
+		private String statusDetails;
+		private Date statusDate;
+		private Address location;
+
 		@Override
 		public String toString() {
 			return "TrackingEvent [objectCreated=" + objectCreated + ", objectUpdated=" + objectUpdated + ", objectId="
@@ -111,21 +113,22 @@ public final class Track extends APIResource {
 		public Address getLocation() {
 			return location;
 		}
-    }
-
-    private TrackingEvent trackingStatus;
-    private TrackingEvent[] trackingHistory;
-
- 	@Override
-	public String toString() {
-		return "Track [carrier=" + carrier + ", tracking_number=" + trackingNumber + ", addressFrom=" + addressFrom + ", addressTo=" + addressTo
-				+ ", eta=" + eta + ", serviceLevel=" + serviceLevel + ", metadata=" + metadata + ", tracking_status=" + trackingStatus 
-				+ ", tracking_history=" + Arrays.toString(trackingHistory) + "]";
 	}
-	
-    public String getCarrier() {
-        return carrier;
-    }
+
+	private TrackingEvent trackingStatus;
+	private TrackingEvent[] trackingHistory;
+
+	@Override
+	public String toString() {
+		return "Track [carrier=" + carrier + ", tracking_number=" + trackingNumber + ", addressFrom=" + addressFrom
+				+ ", addressTo=" + addressTo + ", eta=" + eta + ", serviceLevel=" + serviceLevel + ", metadata="
+				+ metadata + ", tracking_status=" + trackingStatus + ", tracking_history="
+				+ Arrays.toString(trackingHistory) + "]";
+	}
+
+	public String getCarrier() {
+		return carrier;
+	}
 
 	public String getTrackingNumber() {
 		return trackingNumber;
@@ -159,57 +162,57 @@ public final class Track extends APIResource {
 		return trackingHistory;
 	}
 
-    /**
-     * Return URL that maps given tracking number on given carrier as 
-     * https://api.goshippo.com/tracks/<carrier>/<number>
-     */
-    private static String trackingNumberURL(String carrier, String trackingNumber)
+	/**
+	 * Return URL that maps given tracking number on given carrier as
+	 * https://api.goshippo.com/tracks/<carrier>/<number>
+	 */
+	private static String trackingNumberURL(String carrier, String trackingNumber)
 			throws AuthenticationException, InvalidRequestException, APIConnectionException, APIException {
-        try {
-            return String.format("%s/%s/%s", classURL(Track.class), urlEncode(carrier), 
-                                 urlEncode(trackingNumber));
-        } catch (UnsupportedEncodingException e) {
-			throw new InvalidRequestException("Unable to encode parameters to "
-					+ CHARSET
-					+ ". Please contact support@goshippo.com for assistance.",
-					null, e);
-        }
-    }
-	
-    /**
-     * Get tracking information of any package from given carrier.
-     * This corresponds to https://api.goshippo.com/tracks/<carrier>/<tracking number> API defined
-     * in https://goshippo.com/docs/reference#tracks-retrieve
-     *
-     * @param carrier   Name of the carrier (like "usps") tracking the package
-     * @param trackingNumber Tracking number provided by the carrier for a package
-     * @return Track object containing tracking info
-     */
-	public static Track getTrackingInfo(String carrier, String trackingNumber, String apiKey)
-			throws AuthenticationException, InvalidRequestException, APIConnectionException, APIException {
-		return request(RequestMethod.GET, trackingNumberURL(carrier, trackingNumber),
-				null, Track.class, apiKey);
+		try {
+			return String.format("%s/%s/%s", classURL(Track.class), urlEncode(carrier), urlEncode(trackingNumber));
+		} catch (UnsupportedEncodingException e) {
+			throw new InvalidRequestException("Unable to encode parameters to " + CHARSET
+					+ ". Please contact support@goshippo.com for assistance.", null, e);
+		}
 	}
 
-    /**
-     * Register webhook for tracking shipment of a pacakge. This corresponds to API defined in
-     * https://goshippo.com/docs/reference#tracks-create. Please note that the webhook where information
-     * will be posted need to be already provided in https://app.goshippo.com
-     *
-     * @param carrier   Name of the carrier (like "usps") tracking the packing
-     * @param trackingNumber Tracking number provided by the carrier for a package
-     * @param metadata  Generic information related to this tracking
-     * @return Track object containing tracking info
-     */
-    public static Track registerTrackingWebhook(String carrier, String trackingNumber, String metadata, 
-                                                String apiKey)
-			throws AuthenticationException, InvalidRequestException, APIConnectionException, APIException
-    {
+	/**
+	 * Get tracking information of any package from given carrier. This
+	 * corresponds to https://api.goshippo.com/tracks/<carrier>/<tracking
+	 * number> API defined in
+	 * https://goshippo.com/docs/reference#tracks-retrieve
+	 *
+	 * @param carrier
+	 *            Name of the carrier (like "usps") tracking the package
+	 * @param trackingNumber
+	 *            Tracking number provided by the carrier for a package
+	 * @return Track object containing tracking info
+	 */
+	public static Track getTrackingInfo(String carrier, String trackingNumber, String apiKey)
+			throws AuthenticationException, InvalidRequestException, APIConnectionException, APIException {
+		return request(RequestMethod.GET, trackingNumberURL(carrier, trackingNumber), null, Track.class, apiKey);
+	}
+
+	/**
+	 * Register webhook for tracking shipment of a pacakge. This corresponds to
+	 * API defined in https://goshippo.com/docs/reference#tracks-create. Please
+	 * note that the webhook where information will be posted need to be already
+	 * provided in https://app.goshippo.com
+	 *
+	 * @param carrier
+	 *            Name of the carrier (like "usps") tracking the packing
+	 * @param trackingNumber
+	 *            Tracking number provided by the carrier for a package
+	 * @param metadata
+	 *            Generic information related to this tracking
+	 * @return Track object containing tracking info
+	 */
+	public static Track registerTrackingWebhook(String carrier, String trackingNumber, String metadata, String apiKey)
+			throws AuthenticationException, InvalidRequestException, APIConnectionException, APIException {
 		Map<String, Object> params = new HashMap<String, Object>();
-        params.put("carrier", carrier);
-        params.put("tracking_number", trackingNumber);
-        params.put("metadata", metadata);
-        return request(RequestMethod.POST, classURLWithTrailingSlash(Track.class), params,
-                       Track.class, apiKey);
-    }
+		params.put("carrier", carrier);
+		params.put("tracking_number", trackingNumber);
+		params.put("metadata", metadata);
+		return request(RequestMethod.POST, classURLWithTrailingSlash(Track.class), params, Track.class, apiKey);
+	}
 }

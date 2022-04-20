7a43267c676be21c2c2d6ba3351528523e3fe384
Ryan McKay <rmckay@goshippo.com>
2022-04-20 11:02:07 -0500

    GREEN: re-instate old commented-out refund tests

--- src/test/java/com/shippo/model/RefundTest.java
+++ src/test/java/com/shippo/model/RefundTest.java
@@ -13,15 +13,13 @@ import com.shippo.exception.AuthenticationException;
 import com.shippo.exception.InvalidRequestException;
 import com.shippo.exception.ShippoException;
 
-// Test is affected by inability of API to make test purchases
 public class RefundTest extends ShippoTest {
 
-    // Not testable with the current API
-    // @Test
-    // public void testValidCreate() {
-    // Refund testObject = (Refund) getDefaultObject();
-    // assertEquals("QUEUED", testObject.getObject_status());
-    // }
+    @Test
+    public void testValidCreate() {
+        Refund testObject = (Refund) getDefaultObject();
+        assertEquals("QUEUED", testObject.getStatus());
+    }
 
     @Test(expected = InvalidRequestException.class)
     public void testInvalidCreate() throws AuthenticationException, InvalidRequestException, APIConnectionException,
@@ -29,18 +27,17 @@ public class RefundTest extends ShippoTest {
         Refund.create(getInvalidObjectMap());
     }
 
-    // Not testable with the current API
-    // @Test
-    // public void testRetrieve() throws AuthenticationException,
-    // InvalidRequestException, APIConnectionException,
-    // APIException {
-    // Refund testObject = (Refund) getDefaultObject();
-    // Refund retrievedObject;
-    //
-    // retrievedObject = Refund.retrieve((String) testObject.object_id);
-    // assertEquals(testObject.object_id, retrievedObject.object_id);
-    //
-    // }
+    @Test
+    public void testRetrieve() throws AuthenticationException,
+            InvalidRequestException, APIConnectionException,
+            APIException {
+        Refund testObject = (Refund) getDefaultObject();
+        Refund retrievedObject;
+
+        retrievedObject = Refund.retrieve((String) testObject.object_id);
+        assertEquals(testObject.object_id, retrievedObject.object_id);
+
+    }
 
     @Test(expected = InvalidRequestException.class)
     public void testInvalidRetrieve() throws AuthenticationException, InvalidRequestException, APIConnectionException,
@@ -55,23 +52,21 @@ public class RefundTest extends ShippoTest {
         assertNotNull(objectCollection.getData());
     }
 
-    // Not testable with the current API (requires test refunds to exist)
-    // @Test
-    // public void testListPageSize() throws AuthenticationException,
-    // InvalidRequestException, APIConnectionException,
-    // APIException {
-    // Map<String, Object> objectMap = new HashMap<String, Object>();
-    // objectMap.put("results", "1"); // one result per page
-    // objectMap.put("page", "1"); // the first page of results
-    // RefundCollection RefundCollection = Refund.all(objectMap);
-    // assertEquals(RefundCollection.getData().size(), 1);
-    // }
+    @Test
+    public void testListPageSize() throws AuthenticationException,
+            InvalidRequestException, APIConnectionException,
+            APIException {
+        Map<String, Object> objectMap = new HashMap<String, Object>();
+        objectMap.put("results", "1"); // one result per page
+        objectMap.put("page", "1"); // the first page of results
+        RefundCollection RefundCollection = Refund.all(objectMap);
+        assertEquals(RefundCollection.getData().size(), 1);
+    }
 
     public static Object getDefaultObject() {
         Map<String, Object> objectMap = new HashMap<String, Object>();
-        // A test transaction ID does not exist in the current API, making it
-        // impossible to test without purchases
-        objectMap.put("transaction", "a test transaction ID");
+        Transaction transaction = (Transaction) TransactionTest.getDefaultObject();
+        objectMap.put("transaction", transaction.getObjectId());
 
         try {
             Refund testObject = Refund.create(objectMap);

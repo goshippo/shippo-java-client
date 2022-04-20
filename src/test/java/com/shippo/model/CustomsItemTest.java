b3682f06638d6fd70236264e61621bca0158260f
Ryan McKay <rmckay@goshippo.com>
2022-04-20 14:28:53 -0500

    BLUE: more descriptive test fixture method names
    and return types
    for Customs

--- src/test/java/com/shippo/model/CustomsItemTest.java
+++ src/test/java/com/shippo/model/CustomsItemTest.java
@@ -34,7 +34,7 @@ public class CustomsItemTest extends ShippoTest {
 
     @Test
     public void testValidCreate() {
-        CustomsItem testObject = (CustomsItem) getDefaultObject();
+        CustomsItem testObject = createCustomsItemFixture();
         assertEquals(testObject.getObjectState(), "VALID");
         assertEquals(testObject.getDescription(), objectMap.get("description"));
         assertEquals(testObject.getQuantity(), Double.parseDouble((String) objectMap.get("quantity")));
@@ -58,7 +58,7 @@ public class CustomsItemTest extends ShippoTest {
     @Test
     public void testRetrieve() throws AuthenticationException, InvalidRequestException, APIConnectionException,
             APIException {
-        CustomsItem testObject = (CustomsItem) getDefaultObject();
+        CustomsItem testObject = createCustomsItemFixture();
         CustomsItem retrievedObject;
 
         retrievedObject = CustomsItem.retrieve((String) testObject.objectId);
@@ -89,10 +89,9 @@ public class CustomsItemTest extends ShippoTest {
         assertEquals(CustomsItemCollection.getData().size(), 1);
     }
 
-    public static Object getDefaultObject() {
+    public static CustomsItem createCustomsItemFixture() {
         try {
-            CustomsItem testObject = CustomsItem.create(objectMap);
-            return testObject;
+            return CustomsItem.create(objectMap);
         } catch (ShippoException e) {
             e.printStackTrace();
         }
